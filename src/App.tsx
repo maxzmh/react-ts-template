b2282703a0f48f4b81c91fdaad8c53278e222f1e
max <zhangmh1@broada.com>
2022-11-30 16:00:53 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React, { Suspense, lazy } from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import { Button } from "antd";
+
+const Home = lazy(() => import(/* webpackChunkName: 'home' */ "./pages/Home"));
+const About = lazy(() => import(/* webpackChunkName: 'about' */ "./pages/About"));
+
+function App() {
+  return (
+    <div>
+      <h1>App</h1>
+      <Button type="primary">按钮</Button>
+      <ul>
+        <li>
+          <Link to="/home">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <Suspense fallback={<div>loading...</div>}>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
